777098469b6e1c8cfb41a63928c0cd85f316f55c
Arseniy Pendryak <warrior@yandex-team.ru>
2015-03-02 01:23:30 +0300

    created point struct

---
+++ Library/src/main/java/com/ifmo/optimization/methods/multidimensional/Point.java
@@ -0,0 +1,15 @@
+package com.ifmo.optimization.methods.multidimensional;
+
+/**
+ * Created by warrior on 02.03.15.
+ */
+public class Point {
+
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
